f30653c1441a3f2fd83695f83bfbb6c9924c2962
Gabriel Naves <gabri.navess@gmail.com>
2020-04-05 19:32:54 -0300

    Added a way to keep track of where the player is, and moved some torches

---
+++ Assets/Scripts/PlayerFloorArrival.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerFloorArrival : MonoBehaviour {
+
+    public GameObject previousFloorBlockingWall;
+
+    bool playerPassed;
+
+    void Start() {
+        previousFloorBlockingWall.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (!playerPassed && other.gameObject == Player.instance.gameObject) {
+            previousFloorBlockingWall.SetActive(true);
+            Player.instance.currentFloor++;
+            playerPassed = true;
+        }
+    }
+}
